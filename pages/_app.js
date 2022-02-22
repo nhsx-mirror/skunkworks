be053051d1be5103c1102f0a31c2da7429c69b4c
Fraser Cook <fraserc6@gmail.com>
2022-02-22 13:36:59 +0000

    Add meta / social data

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo'
 import Head from 'next/head'
 
 import 'styles/index.css'
@@ -6,7 +7,29 @@ export default function App({ Component, pageProps }) {
    return (
       <>
          <Head>
-            <title>NHS AI Lab Skunkworks | Resources</title>
+            <NextSeo
+               title="NHS AI Lab Skunkworks | Resources"
+               description="Welcome to the technical website of the NHS AI Lab Skunkworks team. For our general public-facing website, please visit the AI Skunkworks programme."
+               openGraph={{
+                  url: 'https://nhsx.github.io/skunkworks/',
+                  title: 'NHS AI Lab Skunkworks',
+                  description: 'Finding new ways to use AI for driving forward the early adoption of technology to support health, in both clinical and business contexts.',
+                  images: [
+                     {
+                        url: 'https://nhsx.github.io/skunkworkssocial-cover.jpg',
+                        width: 1200,
+                        height: 630,
+                        alt: 'NHS AI Lab Skunkworks Social Cover',
+                        type: 'image/jpg',
+                     }
+                  ],
+               }}
+               twitter={{
+                  handle: '@NHSuk',
+                  site: '@NHSuk',
+                  cardType: 'summary_large_image',
+               }}
+            />
          </Head>
          <div className="antialiased">
             <Component {...pageProps} />
--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,8 @@
 import { useMemo } from 'react'
 import Link from 'next/link'
+import { NextSeo } from 'next-seo'
 import { useRouter } from 'next/router'
+import { posts } from 'cache/post-data'
 import Introduction from 'components/Introduction'
 import PageLayout from 'components/layouts/PageLayout'
 
@@ -32,22 +34,28 @@ export default function Search() {
    const results = useMemo(() => q && queryPosts(q), [q])
 
    return (
-      <PageLayout noPagination>
-         <Introduction title="Search Results" description={results ? `Your search returned ${results.length} results` : 'No results found'} />
-         {
-            results && (
-               <div className="space-y-6">
-                  {results.map(result => (
-                     <Link key={result.slug} href={`/${result.slug}`}>
-                        <a className="block">
-                           <h3 className="text-lg font-medium text-nhsuk-text mb-1">{result.title}</h3>
-                           <p className="text-nhsuk-secondary-text">{result.summary}</p>
-                        </a>
-                     </Link>
-                  ))}
-               </div>
-            )
-         }
-      </PageLayout>
+      <>
+         <NextSeo
+            title="NHS AI Lab Skunkworks | Search"
+         />
+
+         <PageLayout noPagination>
+            <Introduction title="Search Results" description={results ? `Your search returned ${results.length} results` : 'No results found'} />
+            {
+               results && (
+                  <div className="space-y-6">
+                     {results.map(result => (
+                        <Link key={result.slug} href={`/${result.slug}`}>
+                           <a className="block">
+                              <h3 className="text-lg font-medium text-nhsuk-text mb-1">{result.title}</h3>
+                              <p className="text-nhsuk-secondary-text">{result.summary}</p>
+                           </a>
+                        </Link>
+                     ))}
+                  </div>
+               )
+            }
+         </PageLayout>
+      </>
    )
 }
